0697fa1deb390f54aae3caa8d4d0fb32ac3bb8de
Eunice <valdezeunice804@gmail.com>
2024-01-26 06:30:17 +0800

    endpoints

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const endPoint =
   process.env.NODE_ENV === "development"
     ? "localhost:3000"
-    : "https://curriculum-evaluation-5da2a2dd9068.herokuapp.com/";
+    : "https://curriculum-evaluation-5da2a2dd9068.herokuapp.com";
